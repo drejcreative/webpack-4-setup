51470058eec31582b7f23cacc59d2234181f7358
drejcreative@gmail.com <drejcreative@gmail.com>
2019-02-25 00:01:12 +0100

    Adding webpage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-const arr = [1, 2, 3];
+import Particles from 'particlesjs';
+import Typed from 'typed.js';
 
-import style from "./main.scss";
-import test from './test.css';
+import "./main.scss";
+import './images/city.jpg';
 
-// const body = document.querySelector('body').style.background = 'blue';
+Particles.init({
+	selector: '.background',
+  color: ['#ffffff', '#3074BB', '#1B2236'],
+  maxParticles: 150,
+  connectParticles: true
+});
+
+const options = {
+  strings: ['It Contain Babel so you can use ES2015 and later versions ', 'You can use CSS and SCSS. It has an Autoprefixer integrated', 'You can use Images, Fonts, and IcoFonts', 'You have Dev Server with live reload'],
+  typeSpeed: 150,
+  startDelay: 1000,
+  loop: true,
+  loopCount: Infinity,
+}
+
+var typed = new Typed(".hero__subheader", options);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 module.exports = {
   module: {
@@ -23,10 +25,14 @@ module.exports = {
       {
         test: /\.css$|.scss$/,
         exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader',]
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
             loader: 'url-loader',
@@ -35,16 +41,11 @@ module.exports = {
             }
           }
         ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
       }
     ]
   },
   plugins: [
+    new ErrorOverlayPlugin(),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
@@ -52,8 +53,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "app.[contenthash].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CleanWebpackPlugin(['dist']),
   ],
+  devtool: 'cheap-module-source-map',
   output: {
     filename: '[name].[contenthash].js',
   }
