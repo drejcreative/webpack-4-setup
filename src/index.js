ca3aef6a3ac0fa7be9c220099fd6c32020bd688e
drejcreative@gmail.com <drejcreative@gmail.com>
2019-02-24 21:54:19 +0100

    Webpack Setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3];
+
+import style from "./main.scss";
+import test from './test.css';
+
+// const body = document.querySelector('body').style.background = 'blue';
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$|.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "app.[contenthash].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+  }
+};
